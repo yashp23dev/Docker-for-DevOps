14186aebfb94a44682fc9ed18f014fb72218d75b
yash <yashppatilk23@gmail.com>
2025-05-22 05:51:33 +0000

    docker compose for multiple images

---
+++ docker_compose_build_image/app.py
@@ -0,0 +1,6 @@
+import os
+
+app_version = os.environ.get('APP_VERSION','Unknown')
+environment = os.environ.get('ENVIRONMENT','Unknown')
+
+print(f"Running App version {app_version} in {environment} environment.")
